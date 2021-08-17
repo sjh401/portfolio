658e9ac72791d48e32b9df925bb8e1a84808b504
stephenharrity <sjh401@gmail.com>
2021-08-17 13:07:26 -0500

    to post

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -8,6 +8,7 @@ export default function Home() {
             <div className='tosubmit'>
                 <div>
                     Welcome!
+                    
                     More coming soon...
                 </div>
                 <div>
