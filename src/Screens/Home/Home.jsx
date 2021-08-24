dd9ed905e1b977f1a169c8d537c1cacd53e811fc
stephenharrity <sjh401@gmail.com>
2021-08-24 13:48:12 -0500

    some more styling

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -3,7 +3,6 @@ import Layout from "../../components/Layout/Layout";
 import './Home.css'
 
 export default function Home() {
-    
     return (
         <Layout>
             <div className="home-div">
@@ -13,22 +12,22 @@ export default function Home() {
                     More coming soon...
                 </div>
                 <div className="home-projects-div">
-                        <div className="home-project-div"  style={{backgroundImage:"url(https://i.imgur.com/T7Zj0rj.png)"}}>
-                            <a href="https://sjh401.github.io/What_Should_You_Drink/">
-                                    <p>General Assembly SEI Project 1</p>
-                                    <p>What Should You Drink</p>
-                                </a>
-                        </div>
-                        <div className="home-project-div"  style={{backgroundImage:"url(https://i.imgur.com/DV2pqoj.png)"}}>
-                            <a href="https://unruffled-khorana-2d5436.netlify.app/">
-                                <p>General Assembly SEI Project 2</p> 
-                                <p>Chick'n'r React Base Game</p>
+                        <div className="home-project-div" >
+                            <a href="https://sjh401.github.io/What_Should_You_Drink/" className="home-project-link">
+                                <img src="https://i.imgur.com/T7Zj0rj.png" className="home-project-img" alt="project 1"/>
+                                <div>General Assembly SEI Project 1: What Should You Drink</div>
                             </a>
                         </div>
-                        <div className="home-project-div" style={{backgroundImage:"url(https://i.imgur.com/5gzMpHW.png)"}}>
-                            <a href="https://stoic-cray-4ca41b.netlify.app/">
-                                <p>General Assembly SEI Project 3</p>
-                                <p>Forumtopia Group MERN Forum</p>
+                        <div className="home-project-div">
+                            <a href="https://unruffled-khorana-2d5436.netlify.app/" className="home-project-link">
+                                <img src="https://i.imgur.com/DV2pqoj.png" className="home-project-img" alt="project 2"/>
+                                <div>General Assembly SEI Project 2: Chick'n'r React Base Game</div>
+                            </a>
+                        </div>
+                        <div className="home-project-div">
+                            <a href="https://stoic-cray-4ca41b.netlify.app/" className="home-project-link">
+                            <img src="https://i.imgur.com/5gzMpHW.png" className="home-project-img" alt="project 3"/>
+                                <div>General Assembly SEI Project 3: Forumtopia Group MERN Forum</div>
                                 </a>
                         </div>
                 </div>
